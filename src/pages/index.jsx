8e63e6df4e91adaf533cc4a1a2f901acc5fd49bb
aguriken <ohagurokenichi@gmail.com>
2023-05-22 08:32:55 +0900

    🚸 APIを叩く際の、ローディング状態・エラー状態を追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,13 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { Links } from 'src/components/Links'
+// import { Links } from 'src/components/Links'
 import { Headline } from 'src/components/Headline'
+import { Posts } from 'src/components/Posts'
+import { useCallback, useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home(props) {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -13,7 +15,7 @@ export default function Home() {
       <Headline onClick={() => alert("clicked!!!")}>
         <code className="font-mono font-bold">src/pages/index.js</code>
       </Headline>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -23,7 +25,17 @@ export default function Home() {
           priority
         />
       </div>
-      <Links />
+      <Links /> */}
+      <Posts />
+      {/* <input type="text" value={props.text} onChange={props.handleChange}></input>
+      <button onClick={props.handleAdd}>追加</button>
+      <ul>
+        {props.array.map(item => {
+          return (
+            <li key={item}>{item}</li>
+          )
+        })}
+      </ul> */}
     </main>
   )
 }
